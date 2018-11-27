29641b037f2a1511756ea645ae03f069e1e5298e
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-11-27 16:02:42 +0000

    first commit

---
+++ src/inject/inject.js
@@ -0,0 +1,15 @@
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Extension starts");
+			// ----------------------------------------------------------
+
+			console.log($)
+		}
+	}, 10);
+});
+
---
+++ tmps/tmp.js
@@ -0,0 +1,5 @@
+https://evnbackend.tumo.lab/activity-try/answers?try_id=5a9fe912cb72c6bda32ad4e4
+
+
+$(".ui-autocomplete-input:last").val("ապրես, լավ աշխատանք է"); 
+$(".btn.btn-success:last").trigger("click");
